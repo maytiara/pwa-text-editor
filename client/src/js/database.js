c8bfe8103dc67c1f1e6f3159939ba1ed0dffbfb3
May Carandang <maytiara@gmail.com>
2022-09-29 14:18:02 +0800

    Started adding logic to database schema by exporting the function using <PUT> request and function to create database connection.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,19 +1,30 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
+
+// creating new database name "type" || Version 1
   openDB('type', 1, {
+
+    // add database schema
     upgrade(db) {
       if (db.objectStoreNames.contains('type')) {
         console.log('type database already exists');
         return;
       }
+
+      // new object stored for the data
       db.createObjectStore('type', { keyPath: 'id', autoIncrement: true });
       console.log('type database created');
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Added logic to a method that accepts content and adds it to the database
+export const putDb = async (content) => {
+  console.log('Update information sent to database');
+
+  // create connection to the database & version of we want to use
+  const typesDb = await openDB('type', 1);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
