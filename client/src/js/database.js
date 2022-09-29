b68010140597a0b796cd9f9d6a77d0f91ed98350
May Carandang <maytiara@gmail.com>
2022-09-29 13:55:48 +0800

    changed the argument parameter/value to "type"

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,14 +1,14 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('type', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('type', { keyPath: 'id', autoIncrement: true });
+      console.log('type database created');
     },
   });
 
