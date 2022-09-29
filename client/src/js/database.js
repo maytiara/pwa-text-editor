ab6ec87023437a75f99235f0448d8ab227f4a379
May Carandang <maytiara@gmail.com>
2022-09-29 14:46:34 +0800

    Added <GETALL> method to get all the data in database. Applied 'readonly' and confirmation to return the result value.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -27,6 +27,7 @@ export const putDb = async (content) => {
 
   // create a new transaction. Inside the argument specified the database name & data function
   // .transaction (middleware)
+  // difference used "readwrite"
   const tx = typeDb.transaction('type', 'readwrite');
 
   // open the bucket stored data
@@ -40,7 +41,30 @@ export const putDb = async (content) => {
   console.log('Success 🚀 Data saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Added logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('Get all information from the database');
+
+  // create connection to the database & version of we want to use
+  const typeDb = await openDB('type', 1);
+
+  // create a new transaction. Inside the argument specified the database name & data function
+  // .transaction (middleware)
+  // difference used "readonly"
+  const tx = typeDb.transaction('type', 'readonly');
+
+  // open the bucket stored data
+  const store = tx.objectStore('type');
+
+  // used <GETALL> method to get all the data in database
+  const request = store.getAll();
+
+  // get confirmation of the req
+  const result = await request;
+  console.log('result.value', result);
+
+  // need a return statement
+  return result;
+};
 
 initdb();
